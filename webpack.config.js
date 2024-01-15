ec98f9363464f5c0085df4f7d2fb9eec67912d5c
Alexandra Paramonova <alpar.wb@gmail.com>
2024-01-15 14:23:34 +0500

    refactor: configure webpack to create separate css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
@@ -28,7 +29,7 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
         exclude: /node_modules/,
       },
       {
@@ -68,5 +69,8 @@ module.exports = {
       ],
     }),
     new ESLintPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
   ],
 };
