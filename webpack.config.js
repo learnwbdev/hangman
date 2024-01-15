a32296bcb4cc0d51ec53814f0dea90499be032c9
Alexandra Paramonova <alpar.wb@gmail.com>
2024-01-15 22:43:37 +0500

    refactor: change webpack config to load svg files as inline svg

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,10 @@ module.exports = {
         type: "asset/resource",
       },
       {
+        test: /\.svg$/i,
+        type: "asset/source",
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
